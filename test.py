d1e3950cf0453b9c24555817960cb57d05170262
MaxQing <1191275859@qq.com>
2022-01-10 15:54:16 +0800

    ->reset.

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-def lazy_sum(*args):
-    def sum():
-        ax = 0
-        for n in args:
-            ax = ax + n
-        return ax
-    return sum
+def closure():
+    fs = []
+    for i in range(1, 4):
+        def f():
+            return i * i
+        fs.append(f)
+    return fs
 
 
-f = lazy_sum(1, 2, 3, 4, 5, 6)
-print(f())
+f1, f2, f3 = closure()
+print(f1())
